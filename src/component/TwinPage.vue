df093b0c0a177fb6825b306dc1202729814e631f
user <user@example.com>
2023-09-22 10:19:30 +0900

    modified by minseok

--- src/component/TwinPage.vue
+++ src/component/TwinPage.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="sensor-page">
-      <iframe class="sensor-iframe" src="http://35.91.41.146:3000/d-solo/ENk1jS84k/test?orgId=1&refresh=5s&from=1694571261713&to=1694592861713&panelId=2" frameborder="0"></iframe>
+      <iframe class="sensor-iframe" src="http://52.36.71.154:3000/d-solo/ENk1jS84k/test?orgId=1&refresh=5s&from=1694571261713&to=1694592861713&panelId=2" frameborder="0"></iframe>
     </div>
   </template>
   
--- src/component/data/co.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <iframe src="http://35.91.41.146:3000/d-solo/ENk1jS84k/test?orgId=1&from=1694571454281&to=1694593054281&refresh=5s&panelId=8" width="450" height="200" frameborder="0"></iframe>
-</template>
-
-<style scoped>
-
-</style>
