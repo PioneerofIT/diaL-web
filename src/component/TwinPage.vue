3c95dad8bbba3aa007c6b0383fd534446fa51109
unknown <rjsgml3736@naver.com>
2023-09-22 18:01:58 +0900

    ud

--- src/component/TwinPage.vue
+++ src/component/TwinPage.vue
@@ -1,21 +1,11 @@
 <template>
-    <div class="sensor-page">
-      <iframe class="sensor-iframe" src="http://52.36.71.154:3000/d-solo/ENk1jS84k/test?orgId=1&refresh=5s&from=1694571261713&to=1694592861713&panelId=2" frameborder="0"></iframe>
+    <div >
+      <iframe  src="http://52.36.71.154:3000/d-solo/ENk1jS84k/test?orgId=1&refresh=5s&from=1694571261713&to=1694592861713&panelId=2" width="100%" height="400" frameborder="0"></iframe>
     </div>
   </template>
   
   <style scoped>
-  .sensor-page {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-  }
+
   
-  .sensor-iframe {
-  width: 450px;
-  height: 200px;
-  border: none; /* 이 부분은 frameborder="0"와 같은 효과를 줍니다. */
-  }
   </style>
   
