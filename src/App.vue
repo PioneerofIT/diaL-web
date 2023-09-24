a439d0c4348e5465a06ce0ed5b76f6e23c0b0485
unknown <rjsgml3736@naver.com>
2023-09-25 05:40:15 +0900

    update API

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,12 @@
         data: null,
         loading: false,
         error: null,
-        assetId: '83ecd1bb-0edb-4b38-9d45-667ac1a500e2'
+        assetId: '83ecd1bb-0edb-4b38-9d45-667ac1a500e2',
+        view: true,
+        city: '',
+        temperature: '',
+        humidity: '',
+        weather:''
       };
     },
     methods: {
@@ -18,19 +23,40 @@
         this.$router.push('/LiveSensorPage');
       }
     },
-    //api endpoint로 부터 lambda 정보 전달
-  }
-  }
+    searchWeather() {
+      const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather?lat=36.34&lon=127.28&appid=66642b3fa015dc2047aff31ed3ff29e4&units=metric';
+;
+      axios.get(BASE_URL)
+      .then((result) => {
+        console.log(result.data);
+        this.city = result.data.name;
+        this.temperature = result.data.main.temp;
+        this.humidity = result.data.main.humidity;
+        this.weather = result.data.weather[0].main;
+        this.view = true;
+      })
+      .catch((error) => {
+      console.error("Error fetching weather data:", error);
+      });
+    },
+  
+  },
+  created() {
+      this.searchWeather();
+    }      
+}
+  
   </script>
   
   
   <template>
     <div >
       <div class="title-container">
-        <h1 class="main-title">CI/CD Testing </h1>
-        <p class="sub-title">타겟 장소: 한밭대학교 N5 302호</p>
-        <div>{{ data }}</div> 
+        <img src = "../public/static/Dial-icon.jpg" alt="Icon Description" class="icon">
+        <h1 class="main-title">diaL 화재 관리자 페이지 </h1>
       </div>
+      <p class="sub-title">타겟 장소: 한밭대학교 N5 302호</p>
+
       <nav>
         <router-link to ="/" class="menu-button"> Home</router-link>
         <router-link to ="/LiveSensorPage" class="menu-button"> 실시간 데이터 </router-link>
@@ -39,6 +65,12 @@
         <router-link to ="/TwinPage" class="menu-button"> 3D 가상 건물 </router-link> 
       </nav>
       <router-view></router-view>
+      <div>
+        <p> 지역 : {{ city  }}</p>
+        <p> 날씨 : {{ weather }}</p>
+        <p> 온도 : {{ temperature }}</p>
+        <p> 습도 {{ humidity  }}%</p>
+      </div>
     </div>
   </template>
     
@@ -62,20 +94,32 @@
     text-align: left;
     padding-left: 20px; /* 왼쪽 간격 추가 */
     margin-bottom: 20px; /* 제목과 <nav> 사이의 간격 추가 */
+    display: flex;
   }
+  .icon {
+  width: 50px;  /* 원하는 크기로 설정 */
+  height: 40px; /* 원하는 크기로 설정 */
+  margin-right: 5px; /* 텍스트와의 간격 조정 */
+}
   
   .main-title {
     color: #42b983; /* 초록색 */
     margin: 0;
     font-size: 24px;
   }
+
+  .main-title .icon {
+    width: 24px;
+    height: 24px;
+    margin-right: 8px; 
+    vertical-align: middle; 
+}
+
   
   .sub-title {
     color: #42b983; /* 초록색 */
-    margin: 5px 0;
+    margin-left: 60px;
     font-size: 16px;
   }
- 
-
   
   </style>
