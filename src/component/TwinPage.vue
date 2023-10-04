e15fb14cc2a99a51566fdb6d029f140b8e822ebb
unknown <rjsgml3736@naver.com>
2023-10-04 17:07:19 +0900

    ..

--- src/component/TwinPage.vue
+++ src/component/TwinPage.vue
@@ -1,6 +1,12 @@
+
+<script>
+
+</script>
+
+
 <template>
     <div >
-      <iframe  src="http://52.36.71.154:3000/d-solo/ENk1jS84k/test?orgId=1&refresh=5s&from=1694571261713&to=1694592861713&panelId=2" width="100%" height="400" frameborder="0"></iframe>
+      <iframe  src="http://52.36.71.154:3000/d-solo/ENk1jS84k/test?orgId=1&refresh=5s&from=1694571261713&to=1694592861713&panelId=2" width="150" height="150" frameborder="0"></iframe>
     </div>
   </template>
   
