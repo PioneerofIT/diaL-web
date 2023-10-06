8b9e6278690cb39a05625e86818b6137bcc66d95
unknown <rjsgml3736@naver.com>
2023-10-06 17:34:40 +0900

    pannel ud

--- src/component/MLPage.vue
+++ src/component/MLPage.vue
@@ -1,6 +1,10 @@
 <template>
     <div class="sensor-page">
-        <iframe src="http://52.36.71.154:3000/d-solo/ENk1jS84k/test?orgId=1&refresh=5s&from=1694573117845&to=1694594717845&panelId=12" width="100%" height="200" frameborder="0"></iframe>
+        <iframe src="http://52.36.71.154:3000/d-solo/ENk1jS84k/test?orgId=1&refresh=5s&from=1694573117845&to=1694594717845&panelId=12" width="100" height="200" frameborder="0"></iframe>
+        <iframe src="http://52.36.71.154:3000/d-solo/ENk1jS84k/test?orgId=1&refresh=5s&from=1696559216895&to=1696580816895&panelId=13" width="100" height="200" frameborder="0"></iframe>
+        <iframe src="http://52.36.71.154:3000/d-solo/ENk1jS84k/test?orgId=1&refresh=5s&from=1696559243189&to=1696580843189&panelId=14" width="100" height="200" frameborder="0"></iframe>
+        <iframe src="http://52.36.71.154:3000/d-solo/ENk1jS84k/test?orgId=1&refresh=5s&from=1696559253174&to=1696580853174&panelId=15" width="100" height="200" frameborder="0"></iframe>
+        <iframe src="http://52.36.71.154:3000/d-solo/ENk1jS84k/test?orgId=1&refresh=5s&from=1696559263776&to=1696580863777&panelId=16" width="100" height="200" frameborder="0"></iframe>
     </div>
   </template>
   
@@ -9,7 +13,7 @@
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 100vh;
+  height: 30vh;
   }
   
   .sensor-iframe {
