22985e37d65a393d392e50d0b4cf810bdc546c6d
unknown <rjsgml3736@naver.com>
2023-11-24 14:31:15 +0900

    twin

--- src/component/TwinPage.vue
+++ src/component/TwinPage.vue
@@ -4,7 +4,7 @@
 <template>
     <div class="container" style= "text-align: center; width: auto; height: auto;">
       <div>
-        <iframe  src="http://52.36.71.154:3000/d-solo/ENk1jS84k/test?orgId=1&refresh=20s&from=1694571261713&to=1694592861713&panelId=2" width="100%" height="400px" frameborder="0"></iframe>
+        <iframe  src="http://52.36.71.154:3000/d-solo/ENk1jS84k/test?orgId=1&refresh=3s&panelId=2" width="100%" height="400px" frameborder="0"></iframe>
       </div>
     </div>  
   </template>
